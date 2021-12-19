d437308c35b844b542b1a3cebc8bccd40449d7d7
verypurple <61333357+verypurple@users.noreply.github.com>
2021-12-19 12:29:49 +0100

    Add project files.

---
+++ Implementation.cs
@@ -0,0 +1,26 @@
+﻿using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+
+namespace WorstGearFirst
+{
+    public class Implementation : MelonMod
+    {
+        internal static GearItem GetWorstItemByName(string name)
+        {
+            var items = new List<GearItem>();
+            GameManager.GetInventoryComponent().GetItems(name, items);
+
+            GearItem worstItem = null;
+
+            foreach (var item in items)
+            {
+                if (!worstItem || item.GetNormalizedCondition() < worstItem.GetNormalizedCondition())
+                {
+                    worstItem = item;
+                }
+            }
+
+            return worstItem;
+        }
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace WorstGearFirst
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(Inventory), "GetHighestConditionGearThatMatchesName")]
+        internal static class Inventory_GetHighestConditionGearThatMatchesName
+        {
+            private static bool Prefix(Inventory __instance, ref GearItem __result, string name)
+            {
+                __result = Implementation.GetWorstItemByName(name);
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(Utils), "GetBestBow")]
+        internal static class Utils_GetBestBow
+        {
+            private static bool Prefix(ref GearItem __result, List<GearItem> items)
+            {
+                __result = Implementation.GetWorstItemByName("GEAR_Bow");
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(Utils), "GetBestTorch")]
+        internal static class Utils_GetBestTorch
+        {
+            private static bool Prefix(ref GearItem __result, List<GearItem> items)
+            {
+                __result = Implementation.GetWorstItemByName("GEAR_Torch");
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(Utils), "GetBestFlareGun")]
+        internal static class Utils_GetBestFlareGun
+        {
+            private static bool Prefix(ref GearItem __result, List<GearItem> items)
+            {
+                __result = Implementation.GetWorstItemByName("GEAR_FlareGun");
+
+                return false;
+            }
+        }
+    }
+}
